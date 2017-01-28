db6ec3fe535c22ef1f19bd800046e93df25991c3
abit <abit@itu.dk>
2017-01-28 23:17:02 +0100

    Introduces use cases for the use of lattice.

--- src/main/scala/eu/abit/lattice/Lattice.scala
+++ src/main/scala/eu/abit/lattice/Lattice.scala
@@ -17,11 +17,19 @@ class LatticeElement[A](val element: Set[A])
 object Test extends App {
   val availExprLat: Lattice[Expression] =
     new Lattice[Expression]((e, e1) => new LatticeElement[Expression](e.element.filter(e1.element.contains(_))), new
-        LatticeElement[Expression](Set(new Variable(1, "bottom")))){}
+        LatticeElement[Expression](Set.empty[Expression])){}
 
   val e1 = new LatticeElement[Expression](Set(new Variable(1, "ds")))
   val e2 = new LatticeElement[Expression](Set(new Variable(1, "ds")))
 
   val e3 = availExprLat.computeCommon(e1, e2)
-  val res = e3.element
+
+  val e4 = new LatticeElement[Int](Set(1))
+  val e5 = new LatticeElement[Int](Set(1,2,3))
+  val constant: Lattice[Int] = new Lattice[Int]((l1, l2) => new LatticeElement[Int](l1.element ++ l2.element), new
+      LatticeElement[Int] (Set.empty[Int])) {}
+
+  val res: LatticeElement[Int] = constant.computeCommon(e4, e5)
+  val l = res.element
+
 }
