1235988b7629b38441c611a31c4cbd1d9c4f8749
abit <abit@itu.dk>
2017-01-28 21:00:19 +0100

    Introduces representation of lattice and its elements.

---
+++ src/main/scala/eu/abit/lattice/Lattice.scala
@@ -0,0 +1,26 @@
+package eu.abit.lattice
+
+import eu.adrianbrink.parser.Expression
+
+/*
+  Intuition: The lattice is used in order to combine information. Thus we can compute the common element of two lattice
+  elements by using the binary relation(op) defined over a Set (polymorphic type [A]).
+  In addition, to instantiate a lattice its bottom element is needed.
+*/
+abstract class Lattice[A](val op: (LatticeElement[A], LatticeElement[A]) => LatticeElement[A],
+                          val bottom: LatticeElement[A]) {
+  def computeCommon(x: LatticeElement[A], y: LatticeElement[A]): LatticeElement[A] = op(x, y)
+}
+
+class LatticeElement[A](val element: Set[A])
+
+object Test extends App {
+  val availExprLat: Lattice[Expression] =
+    new Lattice[Expression]((e, e1) => new LatticeElement[Expression](e.element.filter(e1.element.contains(_))), new
+        LatticeElement[Expression](Set(new Expression(1) {}))){}
+
+  val e1 = new LatticeElement[Expression](Set(new Expression(1) {}))
+  val e2 = new LatticeElement[Expression](Set(new Expression(1) {}))
+
+  val e3 = availExprLat.op(e1, e2)
+}
