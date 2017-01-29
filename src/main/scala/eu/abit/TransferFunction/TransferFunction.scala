fa0a2bbe8736a5d2ad8e3fa35ecde77eba6b3f47
abit <abit@itu.dk>
2017-01-29 22:27:34 +0100

    Introduces the abstraction of the transfer function concept.

---
+++ src/main/scala/eu/abit/TransferFunction/TransferFunction.scala
@@ -0,0 +1,29 @@
+package eu.abit.TransferFunction
+
+import eu.abit.lattice.LatticeElement
+import eu.adrianbrink.parser.{Expression, Statement, Variable}
+
+/*
+  Represents the Transfer Function for each program point.
+ */
+abstract class TransferFunction[A] {
+  /*
+    @variable env: Represents the environment of the analysis
+    @variable latticeElement: Represents the lattice element of the previous/next program point
+    @variable stat: The statement at the program point being analyzed.
+   */
+  def transferFunctionForStatements(env: Map[Variable, LatticeElement[A]])
+                                   (latticeElement: LatticeElement[A])
+                                   (stat: Statement): LatticeElement[A]
+
+  /*
+    @variable env: Represents the environment of the analysis
+    @variable latticeElement: Represents the lattice element of the previous/next program point
+    @variable exp: The expression at the program point being analyzed.
+   */
+  // Not sure if we need that though since we can get the expressions from the statements.
+  def transferFunctionForExpressions(env: Map[Variable, LatticeElement[A]])
+                                    (latticeElement: LatticeElement[A])
+                                    (exp: Expression) =
+            throw new NotImplementedError("This method should be implemented by subclasses....")
+}
